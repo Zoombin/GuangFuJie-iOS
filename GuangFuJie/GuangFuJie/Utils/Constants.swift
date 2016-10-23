393a48a4a1ff0f69fac1db46ac5e8589d71306cf
ycabc1989 <ycabc1989@163.com>
2016-10-23 14:01:49 +0800

    切换线上服务器

--- GuangFuJie/GuangFuJie/Utils/Constants.swift
+++ GuangFuJie/GuangFuJie/Utils/Constants.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class Constants: NSObject {
     //QA
-    static let httpHost = "http://112.124.98.9:3002/api/"
-    static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
-    static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
-    static let isSandBox = true
+//    static let httpHost = "http://112.124.98.9:3002/api/"
+//    static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
+//    static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
+//    static let isSandBox = true
     
     //线上
-//    static let httpHost = "http://112.124.98.9:8002/api/"
-//    static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
-//    static let paySecret = "30e71391-4bca-402b-869e-bc7e118bce6"
-//    static let isSandBox = false
+    static let httpHost = "http://112.124.98.9:8002/api/"
+    static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
+    static let paySecret = "30e71391-4bca-402b-869e-bc7e118bce6"
+    static let isSandBox = false
     
     //本地
 //    static let httpHost = "http://localhost:3000/api"
