ccc24e1ab7a3bfc8191fff5bac67631c44784dc4
yc <ycabc1989@163.com>
2016-12-12 18:11:31 +0800

    详情页面

---
+++ GuangFuJie/GuangFuJie/Controller/2.0/InstallerDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InstallerDetailViewController.swift
+//  GuangFuJie
+//
+//  Created by 颜超 on 2016/12/12.
+//  Copyright © 2016年 颜超. All rights reserved.
+//
+
+import UIKit
+
+class InstallerDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
