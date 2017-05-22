78042340a6badb39c8b04235389d8c58ee148d1d
ycabc1989 <ycabc1989@163.com>
2017-05-22 13:16:21 +0800

    高得光伏替换启动图片

--- GuangFuJie/gaodeguangfu/Utils/Constants.swift
+++ GuangFuJie/gaodeguangfu/Utils/Constants.swift
@@ -16,10 +16,10 @@ class Constants: NSObject {
     static let bannerImageHeight: CGFloat = 313
     
     //QA
-//        static let httpHost = "https://guangfujie.zoombin.com:3002/api/"
-//        static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
-//        static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
-//        static let isSandBox = true
+    static let httpHost = "https://guangfujie.zoombin.com:3002/api/"
+    static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
+    static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
+    static let isSandBox = true
     
     //线上
 //    static let httpHost = "https://guangfujie.zoombin.com:8002/api/"
@@ -28,10 +28,10 @@ class Constants: NSObject {
 //    static let isSandBox = false
     
     //本地
-        static let httpHost = "http://localhost:3002/api/"
-        static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
-        static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
-        static let isSandBox = true
+//        static let httpHost = "http://localhost:3002/api/"
+//        static let payKey = "4d6af0de-2542-433e-a361-73ef59805d35"
+//        static let paySecret = "3f339c0c-4c11-464b-b8bd-8fad7ab3717c"
+//        static let isSandBox = true
     
     static let osType = 1 //ios
     static let aeskey = "e41cd1f755f211e6b56b00ff8821fdcf"
