d4cda0a2ac43f40090adc28de88c650d6af7de0b
ycabc1989 <ycabc1989@163.com>
2017-07-06 09:30:34 +0800

    重新创建项目

---
+++ GuangFuJie/GuangFuJie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuangFuJie
+//
+//  Created by 颜超 on 2017/7/6.
+//  Copyright © 2017年 yc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
