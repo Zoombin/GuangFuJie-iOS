c64401559735fe52c08dfb88cd6adf37b31de9aa
ycabc1989 <ycabc1989@163.com>
2017-07-11 09:23:42 +0800

    保险列表

---
+++ GuangFuJie/GuangFuJie/Controller/InsuranceListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  InsuranceListViewController.swift
+//  GuangFuJie
+//
+//  Created by 颜超 on 2017/7/11.
+//  Copyright © 2017年 yc. All rights reserved.
+//
+
+import UIKit
+
+class InsuranceListViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var inTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InsuranceCell")
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
