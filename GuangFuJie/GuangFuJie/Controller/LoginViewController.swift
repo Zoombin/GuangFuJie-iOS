185713676465b7aab69419133c01e4186a2cd8ba
ycabc1989 <ycabc1989@163.com>
2017-07-11 11:34:16 +0800

    登录页面

--- GuangFuJie/GuangFuJie/Controller/LoginViewController.swift
+++ GuangFuJie/GuangFuJie/Controller/LoginViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class LoginViewController: BaseViewController {
-
+class LoginViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var loginTableView: UITableView!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var codeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +24,68 @@ class LoginViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LoginCell_\(indexPath.row + 1)")
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    /**
+     登录页面代理方法--获取验证码
+     */
+    @IBAction func getCodeButtonClicked() {
+        let phone = ""
+        
+        if (phone.isEmpty) {
+            self.showHint("请输入手机号!")
+            return
+        }
+        self.showHud(in: self.view, hint: "验证码获取中...")
+        API.sharedInstance.userCaptcha(phone, success: { (commonModel) in
+            self.hideHud()
+            self.showHint("验证码将发送到您的手机!")
+        }) { (msg) in
+            self.hideHud()
+            self.showHint(msg)
+        }
+    }
+    
+    /**
+     登录页面代理方法--登录按钮
+     
+     - parameter phone
+     - parameter code
+     */
+    @IBAction func loginButtonClicked() {
+        let phone = ""
+        let code = ""
+        
+        if (phone.isEmpty) {
+            self.showHint("请输入手机号!")
+            return
+        }
+        if (code.isEmpty) {
+            self.showHint("请输入验证码!")
+            return
+        }
+        self.showHud(in: self.view, hint: "登录中...")
+        API.sharedInstance.login(phone, captcha: code, success: { (userinfo) in
+            self.hideHud()
+            self.showHint("登录成功!")
+            UserDefaultManager.saveString(UserDefaultManager.USER_INFO, value: userinfo.mj_JSONString())
+        }) { (msg) in
+            self.hideHud()
+            self.showHint(msg)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
