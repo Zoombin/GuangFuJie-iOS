8d35ea208cccd73c1e547eae3c2ae3b52f861534
ycabc1989 <ycabc1989@163.com>
2017-07-15 14:11:40 +0800

    安装商申请、业主申请、地推申请、加盟商申请

---
+++ GuangFuJie/GuangFuJie/Controller/YeZhuApplyViewController.swift
@@ -0,0 +1,73 @@
+//
+//  YeZhuApplyViewController.swift
+//  GuangFuJie
+//
+//  Created by 颜超 on 2017/7/15.
+//  Copyright © 2017年 yc. All rights reserved.
+//
+
+import UIKit
+
+//业主申请
+class YeZhuApplyViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var applyTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "业主申请"
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let submitBottomView = UIView.init(frame: CGRect(x: 0, y: PhoneUtils.kScreenHeight - 50, width: PhoneUtils.kScreenWidth, height: 50))
+        submitBottomView.backgroundColor = UIColor.white
+        self.view.addSubview(submitBottomView)
+        
+        let buttonWidth = PhoneUtils.kScreenWidth - 5 * 2
+        let buttonHeight = submitBottomView.frame.size.height - 5 * 2
+        
+        let submitButton = GFJBottomButton.init(type: UIButtonType.custom)
+        submitButton.frame = CGRect(x: 5, y: 5, width: buttonWidth, height: buttonHeight)
+        submitButton.setTitle("提交审核", for: UIControlState.normal)
+        submitButton.backgroundColor = Colors.appBlue
+        submitButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        submitButton.titleLabel?.font = UIFont.systemFont(ofSize: FontUtils.getFontSize(size: 17))
+        submitButton.addTarget(self, action: #selector(self.submit), for: UIControlEvents.touchUpInside)
+        submitBottomView.addSubview(submitButton)
+    }
+    
+    func submit() {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell_\(indexPath.section)_\(indexPath.row + 1)")
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
