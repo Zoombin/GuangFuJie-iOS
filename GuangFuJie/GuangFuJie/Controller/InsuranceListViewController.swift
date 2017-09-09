84338a15884763c1b6002c39616cae3af23eaa7c
ycabc1989 <ycabc1989@163.com>
2017-09-09 12:12:58 +0800

    增加购买保险列表的标题

--- GuangFuJie/GuangFuJie/Controller/InsuranceListViewController.swift
+++ GuangFuJie/GuangFuJie/Controller/InsuranceListViewController.swift
@@ -13,7 +13,7 @@ class InsuranceListViewController: BaseViewController, UITableViewDataSource, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "购买保险列表"
         // Do any additional setup after loading the view.
     }
     
