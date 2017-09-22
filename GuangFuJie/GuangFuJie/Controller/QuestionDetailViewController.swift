b812f756fffc694fbc2bd5e7b23d3f075b9305b8
ycabc1989 <ycabc1989@163.com>
2017-09-22 16:24:15 +0800

    光伏问答详情

--- GuangFuJie/GuangFuJie/Controller/QuestionDetailViewController.swift
+++ GuangFuJie/GuangFuJie/Controller/QuestionDetailViewController.swift
@@ -10,33 +10,84 @@ import UIKit
 
 class QuestionDetailViewController: BaseViewController {
     var questionInfo: QuestionInfo?
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var createTimeLabel: UILabel!
-    @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var answerLabel: UILabel!
-    @IBOutlet weak var updateLabel: UILabel!
+    
+    var fontOffSet: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "问题详情"
-        initView()
-        loadData()
-    }
-    
-    func loadData() {
-        if (questionInfo != nil) {
-            titleLabel.text = questionInfo!.question
-            createTimeLabel.text = "提问时间：\(questionInfo!.created_date!)"
-            answerLabel.text = questionInfo!.answer
-            updateLabel.text = "解答时间：\(questionInfo!.update_date!)"
+        if (PhoneUtils.kScreenWidth == 375) {
+            fontOffSet = 0
+        } else if (PhoneUtils.kScreenWidth == 320) {
+            fontOffSet = -2
+        } else {
+            fontOffSet = 2
         }
+        initView()
     }
     
     func initView() {
+        let scrollView = UIScrollView.init(frame: CGRect(x: 0, y: 0, width: PhoneUtils.kScreenWidth, height: PhoneUtils.kScreenHeight))
+        self.view.addSubview(scrollView)
+        
+        let times = PhoneUtils.kScreenWidth / 375
+        let offSetX: CGFloat = 5 * times
+        let offSetY: CGFloat = 5 * times
+        let iconWidth: CGFloat = 17 * times
+        let iconHeight: CGFloat = 15 * times
+        let bkgView = UIView.init(frame: CGRect(x: offSetX, y: offSetY, width: PhoneUtils.kScreenWidth - offSetX * 2, height: 0))
+        bkgView.backgroundColor = UIColor.white
+        scrollView.addSubview(bkgView)
+        
+        let width = bkgView.frame.size.width
+        let questionLabelHeight = MSLFrameUtil.getLabHeight(questionInfo!.question, font: UIFont.systemFont(ofSize: 15 + fontOffSet), width: bkgView.frame.size.width - 2 * offSetX)
+        let answerHeight = MSLFrameUtil.getLabHeight(questionInfo!.answer, font: UIFont.systemFont(ofSize: 15 + fontOffSet), width: bkgView.frame.size.width - offSetX * 3 - iconWidth)
+        
+        let questionLabel = UILabel.init(frame: CGRect(x: offSetX, y: 0, width: width - 2 * offSetX, height: questionLabelHeight))
+        questionLabel.textAlignment = NSTextAlignment.left
+        questionLabel.numberOfLines = 0;
+        questionLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        questionLabel.text = questionInfo!.question
+        questionLabel.font = UIFont.systemFont(ofSize: 17 + fontOffSet)
+        bkgView.addSubview(questionLabel)
+        
+        let askDateLabel = UILabel.init(frame: CGRect(x: offSetX, y: questionLabel.frame.maxY, width: width - offSetX, height: 20))
+        askDateLabel.text = "提问时间：\(questionInfo!.created_date!.substring(to: String.Index(10)))"
+        askDateLabel.font = UIFont.systemFont(ofSize: 13 + fontOffSet)
+        askDateLabel.textColor = UIColor.lightGray
+        bkgView.addSubview(askDateLabel)
+        
+        let statusLabel = UILabel.init(frame: CGRect(x: offSetX, y: askDateLabel.frame.maxY + offSetY, width: 57 * times, height: 27 * times))
+        statusLabel.text = questionInfo!.is_answered!.intValue == 1 ? "已解答" : "未解答"
+        statusLabel.textAlignment = NSTextAlignment.center
+        statusLabel.textColor = questionInfo!.is_answered!.intValue == 1 ? Colors.answerGreen : UIColor.red
         statusLabel.layer.cornerRadius = 6.0
         statusLabel.layer.masksToBounds = true
         statusLabel.layer.borderColor = statusLabel.textColor.cgColor
         statusLabel.layer.borderWidth = 0.5
+        statusLabel.font = UIFont.systemFont(ofSize: 13 + fontOffSet)
+        bkgView.addSubview(statusLabel)
+        
+        let line = UILabel.init(frame: CGRect(x: 0, y: statusLabel.frame.maxY + offSetY, width: bkgView.frame.size.width, height: 0.5))
+        line.backgroundColor = Colors.bkgColor
+        bkgView.addSubview(line)
+        
+        let imageIcon = UIImageView.init(frame: CGRect(x: (offSetX * 3 + iconWidth - iconWidth) / 2, y: line.frame.maxY + offSetY * 2.2, width: iconWidth, height: iconHeight))
+        imageIcon.image = UIImage(named: "ic_gfask_repeat")
+        bkgView.addSubview(imageIcon)
+        
+        let contentLabel = UILabel.init(frame: CGRect(x: iconWidth + offSetX * 3, y: line.frame.maxY + offSetY, width: width - offSetX * 3 - iconWidth, height: answerHeight))
+        contentLabel.textAlignment = NSTextAlignment.left
+        contentLabel.text = questionInfo!.answer
+        contentLabel.textColor = UIColor.lightGray
+        contentLabel.numberOfLines = 0;
+        contentLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        contentLabel.font = UIFont.systemFont(ofSize: 15 + fontOffSet)
+        bkgView.addSubview(contentLabel)
+        
+        bkgView.frame = CGRect(x: bkgView.frame.origin.x, y: bkgView.frame.origin.y, width: bkgView.frame.size.width, height: contentLabel.frame.maxY + offSetY)
+        
+        scrollView.contentSize = CGSize(width: 0, height: bkgView.frame.maxY + 5)
     }
 
     override func didReceiveMemoryWarning() {
--- GuangFuJie/GuangFuJie/Utils/Colors.swift
+++ GuangFuJie/GuangFuJie/Utils/Colors.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class Colors: NSObject {
-    static let appBlue : UIColor = UIColor(colorLiteralRed: 36/255.0, green: 131/255.0, blue: 203/255.0, alpha: 1.0)
-    static let bkgColor : UIColor = UIColor(colorLiteralRed: 236/255.0, green: 236/255.0, blue: 236/255.0, alpha: 1.0)
+    static let appBlue : UIColor = UIColor.init(red: 36/255.0, green: 131/255.0, blue: 203/255.0, alpha: 1.0)
+    static let bkgColor : UIColor = UIColor.init(red: 236/255.0, green: 236/255.0, blue: 236/255.0, alpha: 1.0)
     
     static let calUnSelectColor : UIColor = UIColor.white
     static let calSelectedColor : UIColor = UIColor.init(hexString: "F1F1F1")
     static let calUnSelectTextColor : UIColor = UIColor.black
     static let calSelectedTextColor : UIColor = UIColor.init(hexString: "2483CB")
     
-    static let installColor : UIColor = UIColor(colorLiteralRed: 74/255.0, green: 178/255.0, blue: 230/255.0, alpha: 1.0)
-    static let installRedColor : UIColor = UIColor(colorLiteralRed: 255/255.0, green: 18/255.0, blue: 18/255.0, alpha: 1.0)
+    static let installColor : UIColor = UIColor.init(red: 74/255.0, green: 178/255.0, blue: 230/255.0, alpha: 1.0)
+    static let installRedColor : UIColor = UIColor.init(red: 255/255.0, green: 18/255.0, blue: 18/255.0, alpha: 1.0)
     
-    static let lightBlue : UIColor = UIColor(colorLiteralRed: 232/255.0, green: 244/255.0, blue: 253/255.0, alpha: 1.0)
-    static let lightYellow : UIColor = UIColor(colorLiteralRed: 253/255.0, green: 249/255.0, blue: 232/255.0, alpha: 1.0)
+    static let lightBlue : UIColor = UIColor.init(red: 232/255.0, green: 244/255.0, blue: 253/255.0, alpha: 1.0)
+    static let lightYellow : UIColor = UIColor.init(red: 253/255.0, green: 249/255.0, blue: 232/255.0, alpha: 1.0)
+    
+    static let answerGreen : UIColor = UIColor.init(red: 125/255.0, green: 191/255.0, blue: 125/255.0, alpha: 1.0)
 }
